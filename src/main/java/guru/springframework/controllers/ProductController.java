bd9dfbd9ef7fc71b6ac4ae7c1320663ac791a4ed
bessam <sahlibessam@gmail.com>
2019-04-04 12:07:18 +0100

    AUTOMATED THE IT TESTS SO IT START THE CONTAINERS RUN THE TESTS AND SHUT THEM DOWN

--- src/main/java/guru/springframework/controllers/ProductController.java
+++ src/main/java/guru/springframework/controllers/ProductController.java
@@ -7,8 +7,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.Date;
-import java.util.UUID;
 
 /**
  * Created by jt on 1/20/16.
